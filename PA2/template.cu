24e7e139c05b2f88c8016d06be47abb4278ed52d
Yahoo <yac033@ucsd.edu>
2023-01-22 00:12:17 -0800

    grid size

--- PA2/template.cu
+++ PA2/template.cu
@@ -39,6 +39,9 @@ int main(int argc, char **argv) {
   gpuTKTime_stop(GPU, "Copying input memory to the GPU.");
 
   //@@ Initialize the grid and block dimensions here
+  dim3 grid_size(n/256, 1, 1);
+  if (n%256) grid_size.x++;
+  dim3 block_size(256, 1, 1);
 
   gpuTKTime_start(Compute, "Performing CUDA computation");
   //@@ Launch the GPU Kernel here
